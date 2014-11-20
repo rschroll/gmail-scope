4800248f00e298a04dec3143900323462b6aa7ca
Robert Schroll <rschroll@gmail.com>
2014-11-19 21:53:45 -0500

    Widen date image
    This should be wide enough for all English dates.  I'm a bit worried
    about other languages, though.  Closes #11.

--- include/scope/svg.h
+++ include/scope/svg.h
@@ -1,3 +1,3 @@
-#define SVG_FRAGMENT_1 "<?xml version='1.0' encoding='UTF-8' standalone='no'?>\n<svg xmlns='http://www.w3.org/2000/svg' width='100' height='23' id='svg2' version='1.1'> <text xml:space='preserve' style='font-size:20px;font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;text-anchor:end;line-height:100%;letter-spacing:0px;word-spacing:0px;fill:"
-#define SVG_FRAGMENT_2 ";fill-opacity:1;stroke:none;font-family:Ubuntu;' x='100' y='18' id='text3007'><tspan id='tspan3009' x='100' y='18'>"
+#define SVG_FRAGMENT_1 "<?xml version='1.0' encoding='UTF-8' standalone='no'?>\n<svg xmlns='http://www.w3.org/2000/svg' width='120' height='23' id='svg2' version='1.1'> <text xml:space='preserve' style='font-size:20px;font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;text-anchor:end;line-height:100%;letter-spacing:0px;word-spacing:0px;fill:"
+#define SVG_FRAGMENT_2 ";fill-opacity:1;stroke:none;font-family:Ubuntu;' x='120' y='18' id='text3007'><tspan id='tspan3009' x='120' y='18'>"
 #define SVG_FRAGMENT_3 "</tspan></text> </svg>"
